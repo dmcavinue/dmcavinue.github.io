7800890d33c08e3670e452fc91212ef168541475
dmcavinue <dan@mcavinue.io>
2026-07-15 19:40:34 -0400

    add nix flake update deps

--- flake.nix
+++ flake.nix
@@ -1,16 +1,34 @@
 {
   inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nix-environments.url = "git+ssh://git@github.com/dmcavinue/nix-environments.git";
   };
   outputs = { self, nixpkgs, nix-environments }:
     let
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
+
+      ruby = pkgs.ruby_3_4;
+
+      gems = pkgs.bundlerEnv {
+        name = "jekyll-env";
+        inherit ruby;
+        gemdir = ./docs; 
+      };
+
       m = nix-environments.devShellModules;
     in {
       devShells.x86_64-linux.default = nix-environments.lib.mkShell pkgs [
-        (m.base { inherit pkgs; extraPkgs = [
-          pkgs.bashInteractive
-        ];})
+        (m.base { 
+          inherit pkgs; 
+          extraPkgs = [
+            pkgs.bashInteractive
+            ruby
+            gems
+            gems.wrappedRuby
+            pkgs.bundler
+            pkgs.bundix
+          ];
+        })
         (m.opencode { inherit pkgs; })
       ];
     };
